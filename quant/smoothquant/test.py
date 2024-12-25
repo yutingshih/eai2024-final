3506bf6051e364d6c2ff9cdd0ad8ba2afc11554b
Yu-Ting Shi <nn6124030@gs.ncku.edu.tw>
2024-12-25 20:08:46 +0800

    feat: add support for both INT8 and FP16 model evaluation in smoothquant/test.py

--- quant/smoothquant/test.py
+++ quant/smoothquant/test.py
@@ -1,19 +1,30 @@
 import argparse
 import os
 import sys
-project_root = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(project_root, "smoothquant"))
+
+# project_root = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(os.path.join(project_root, "smoothquant"))
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 import torch
 from torch import nn
-from transformers import AutoTokenizer,AutoModelForCausalLM, LlamaTokenizer
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    LlamaTokenizer,
+    LlamaForCausalLM,
+)
 from datasets import load_dataset
 from smoothquant.fake_quant import W8A8Linear
-# from smoothquant.smoothquant.fake_quant import W8A8Linear
+
 import tqdm
 
+DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available() else "cpu"
+)
 
 
 class Evaluator:
@@ -55,49 +66,82 @@ def parse_args():
         default="../../weights/smoothquant/llama2-smooth-w8a8.pt",
         help="Path to the PyTorch model to be evaluated (*.pt)",
     )
-    # parser.add_argument(
-    #  "--tokenizer",
-    #   default="../../weights/meta-llama/Llama-2-7b-hf",
-    #    help="Directory to the tokenizer",
-    # )
+    parser.add_argument(
+        "--tokenizer",
+        default="../../weights/meta-llama/Llama-2-7b-hf",
+        help="Directory to the tokenizer",
+    )
+    parser.add_argument(
+        "-q",
+        "--quantized",
+        action="store_true",
+        help="Indicate if the model loaded is quantized",
+    )
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="Show more information"
     )
     args = parser.parse_args()
 
-    if not os.path.isfile(args.model):
+    if args.quantized and not os.path.isfile(args.model):
         raise ValueError(f"--model argument should be a file. got {args.model}")
-    # if not os.path.isdir(args.tokenizer):
-      #  raise ValueError(
-      #      f"--tokenizer argument should be a directory. got {args.tokenizer}"
-       # )
+    elif not args.quantized and not os.path.isdir(args.model):
+        raise ValueError(f"--model argument should be a directory. got {args.model}")
+
+    if not os.path.isdir(args.tokenizer):
+        raise ValueError(
+            f"--tokenizer argument should be a directory. got {args.tokenizer}"
+        )
 
     return args
 
 
-def main():
-    args = parse_args()
-    os.makedirs(os.path.dirname(os.path.abspath(args.model)), exist_ok=True)
-    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"); print(f"Using device: {torch.cuda.get_device_name(0) if device.type == 'cuda' else 'MPS' if device.type == 'mps' else 'CPU'}")
+def evaluate_fp16_model(model_path: str, evaluator: Evaluator) -> float:
+    model = LlamaForCausalLM.from_pretrained(
+        model_path, torch_dtype=torch.float16, device_map="auto"
+    )
 
-    # tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-    dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-    evaluator = Evaluator(dataset, tokenizer, device)
+    print(f"Model loaded from {model_path}")
+    if verbose:
+        print(model)
 
+    ppl = evaluator.evaluate(model)
+    print(f"FP16 model perplexity: {ppl}")
+    return ppl
+
+
+def evaluate_int8_model(model_path: str, evaluator: Evaluator) -> float:
     W8A8Linear.nop = lambda self, x: x
 
     # you need to map the model to the CPU first instead of directly loading it onto the MPS device
-    # model = torch.load(args.model)
-    model = torch.load(args.model, map_location=torch.device('cpu'))
-    model = model.to(device)
-    
-    print(f"Model loaded from {args.model}")
-    if args.verbose:
+    model = torch.load(model_path, weights_only=False, map_location=torch.device("cpu"))
+    model = model.to(DEVICE)
+
+    print(f"Model loaded from {model_path}")
+    if verbose:
         print(model)
 
     ppl = evaluator.evaluate(model)
     print(f"SmoothQuant W8A8 quantized model perplexity: {ppl}")
+    return ppl
+
+
+def main():
+    args = parse_args()
+    global verbose
+    verbose = args.verbose
+
+    print(
+        f"Using device: {torch.cuda.get_device_name(0) if DEVICE == 'cuda' else DEVICE.upper()}"
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+    dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+    evaluator = Evaluator(dataset, tokenizer, DEVICE)
+
+    if args.quantized:
+        evaluate_int8_model(model_path=args.model, evaluator=evaluator)
+    else:
+        evaluate_fp16_model(model_path=args.model, evaluator=evaluator)
 
 
 if __name__ == "__main__":
